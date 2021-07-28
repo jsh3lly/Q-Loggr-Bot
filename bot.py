04e569c49918aad848856eae4b5e1408d554dcde
StaticESC <slushiboy21@gmail.com>
2021-07-28 15:07:05 -0600

    test commit

--- bot.py
+++ bot.py
@@ -70,7 +70,7 @@ helpList = [embedHelp, embedSave, embedFetch, embedPlayl]
 
 
 #----------------------------
-# test commit text
+# test commit text /
 def main():
     load_dotenv()
     TOKEN = os.getenv('DISCORD_TOKEN')
